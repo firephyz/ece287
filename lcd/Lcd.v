79e749dbba1d73211c187b24a28a56f99f591b98
unknown <burgekm@CEC-EGB267-10.it.muohio.edu>
2016-10-27 16:58:36 -0400

    More lcd modifications

--- lcd/Lcd.v
+++ lcd/Lcd.v
@@ -1,4 +1,4 @@
-module Lcd(test, start, data_out_pin, rw_pin, rs_pin, power_pin, enable_pin, clk);
+module Lcd(test, com, go_pin, data_out_pin, rw_pin, rs_pin, power_pin, enable_pin, clk);
 
 	parameter
 		RS_INSTRUCTION = 0,
@@ -13,14 +13,13 @@ module Lcd(test, start, data_out_pin, rw_pin, rs_pin, power_pin, enable_pin, clk
 		Control_Show_Cursor_Blink 	= 8'b00001111,
 		Func_8bit_2line_NormFont	= 8'b00110000;
 	
-	input start;
+	input go_pin;
 	input clk;													// clock signal
 	inout [7:0]data_out_pin;								// Data lines to LCD display
 	output enable_pin, rw_pin, rs_pin, power_pin;	// Various control related wires to the display
 	
 	// Connect internal regs with output wires
 	reg power;
-	reg enable;
 	reg rw;
 	reg rs;
 	assign enable_pin = enable;
@@ -31,12 +30,16 @@ module Lcd(test, start, data_out_pin, rw_pin, rs_pin, power_pin, enable_pin, clk
 	// Extra internal wires
 	wire busy_signal;
 	reg busy_signal_storage;
-	wire [9:0]com;
-	reg has_init;
+	output wire [9:0]com;
 	reg busyTick;
 	reg [9:0] commands[0:15];
 	reg [15:0] instrCounter;
 	reg [7:0] data_out_reg;
+	reg enable;
+	
+	reg[31:0]start_counter;
+	reg [7:0]counter;
+	reg start;
 	
 	// Connect the wire we will probe to see if the module is busy
 	assign data_out_pin = busyTick ? 8'bZ : data_out_reg;
@@ -46,68 +49,87 @@ module Lcd(test, start, data_out_pin, rw_pin, rs_pin, power_pin, enable_pin, clk
 	output reg test;
 	
 	initial begin
-		power = 1;
-		has_init = 0;
-		data_out_reg = 8'bZ;
-		enable = 1;
-		rw = 0;
-		rs = 0;
-		busyTick = 0;
-		busy_signal_storage = 0;
+		power <= 1;
+		data_out_reg <= com[7:0];
+		rw <= 1;
+		rs <= 0;
+		busyTick <= 1;
+		busy_signal_storage <= 0;
+		enable <= 1;
 		
-		instrCounter = 16'd0;
-		commands[0] = 8'b0000000001;
-		commands[1] = 8'b0000000110;
-		commands[2] = 8'b0000001111;
-		commands[3] = 8'b0000110000;
+		start <= 0;
+		counter <= 8'd0;
+		start_counter <= 32'd0;
 		
-		test = 0;
+		test <= 0;
+		
+		instrCounter <= 16'd0;
+		commands[0] <= 8'b0000000001;
+		commands[1] <= 8'b0000000110;
+		commands[2] <= 8'b0000001111;
+		commands[3] <= 8'b0000110000;
+		commands[4] <= 8'b1000110000;
 	end
-
+	
 	// Main data loop
 	always @(posedge clk) begin
 		
+		// Wait for the switch to start the init process
+		if(go_pin == 1) begin
+			start <= 1;
+		end
+		else begin
+			start <= start;
+		end
+		
 		if(start) begin
-			// Initialize the display
-			if(!has_init) begin
+			counter <= counter + 8'b1;
 			
-				busyTick = !busyTick;
+			if (counter == 8'd30 || counter == 8'd60) begin
+				enable <= !enable;
+			end
+			
+			if (counter == 8'd60) begin
+				counter <= 8'd0;
+				
+				busyTick <= !busyTick;
 			
 				if(busyTick) begin
 					//rw = RW_READ;
-					rs = RS_INSTRUCTION;
-					instrCounter = instrCounter;
-					busy_signal_storage = data_out_pin[7];
-					
-					if(test) begin
-						rw = RW_WRITE;
-						data_out_reg = data_out_reg;
-					end
-					else begin
-						rw = RW_READ;
-						data_out_reg = 8'bZ;
-					end
+					rs <= RS_INSTRUCTION;
+					instrCounter <= instrCounter;
+					busy_signal_storage <= data_out_pin[7];
+					rw <= RW_READ;
+					data_out_reg <= data_out_reg;
 				end
 				else begin
 				
-					busy_signal_storage = busy_signal_storage;
+					busy_signal_storage <= busy_signal_storage;
 				
 					if(!busy_signal) begin
-						rs = com[9];
-						rw = com[8];
-						data_out_reg = com[7:0];
-						//instrCounter = instrCounter + 16'b1;
-						instrCounter = instrCounter;
-						test = 1;
+						if(instrCounter < 16'd1) begin
+							test <= 1;
+							instrCounter <= instrCounter + 16'b1;
+							rs <= com[9];
+							rw <= com[8];
+							data_out_reg <= com[7:0];
+						end
+						else begin
+							instrCounter <= instrCounter;
+							rs <= RS_INSTRUCTION;
+							rw <= RW_READ;
+							data_out_reg <= data_out_reg;
+						end
 					end
 					else begin
-						rw = RW_READ;
-						rs = RS_INSTRUCTION;
-						data_out_reg = 8'bZ;
-						instrCounter = instrCounter;
+						rw <= RW_READ;
+						rs <= RS_INSTRUCTION;
+						data_out_reg <= 8'bZ;
+						instrCounter <= instrCounter;
 					end
 				end
 			end
 		end
 	end
+	
 endmodule
--- lcd/simulation/qsim/work/@lcd_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Lcd_vlg_vec_tst is
-end Lcd_vlg_vec_tst;
